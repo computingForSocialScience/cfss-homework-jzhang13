9213c3086effab87e6b11c87480d9987f57be2e4
jzhang13 <jzhang13@uchicago.edu>
2015-02-22 03:00:49 -0600

    asdf

--- Assignment6/analyzeNetworks.py
+++ Assignment6/analyzeNetworks.py
@@ -1,38 +1,35 @@
-import sys
 import requests
-import csv
 import pandas as pd
 import networkx as nx
 import numpy
 
 def readEdgeList(filename):
-    edgelist = pd.read_csv(filename)
-    if length(edgelist.columns) != 2:
+    edgeList = pd.read_csv(filename)
+    if len(edgeList.columns) != 2:
         print "Warning: csv does not contain 2 columns"
-        return edgelist.iloc[:, :2]
-    return edgelist
+        return edgeList.iloc[:, :2]
+    return edgeList
 
 def degree(edgeList, in_or_out):
-    if in_or_out == 'in':
+    if in_or_out is 'in':
         return edgeList['1'].value_counts()
     return edgeList['0'].value_counts()
 
-def combineEdgelists(edgeList1, edgeList2):
-    return pd.concat(edgeList1, edgeList2).drop_duplicates()
+def combineEdgeLists(edgeList1, edgeList2):
+    return pd.concat([edgeList1, edgeList2]).drop_duplicates()
 
 def pandasToNetworkX(edgeList):
     tps = edgeList.to_records(index=False)
     dg = nx.DiGraph()
-    dg = dg.add_edges_from(tps)
+    dg.add_edges_from(tps)
     return dg
 
 def randomCentralNode(inputDiGraph):
-    dg = inputDiGraph
-    evc = nx.eigenvector_centrality(dg)
+    evc = nx.eigenvector_centrality(inputDiGraph)
     sm = sum(evc.values())
     nevc = {}
     for key, value in evc.items():
         nevc[key] = value / sm
-    randn = numpy.random.choice(nevc.keys(), p=nevc.values())
-    return randn
+    return numpy.random.choice(nevc.keys(), p=nevc.values())
     
+
